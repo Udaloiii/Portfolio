f55de4d399937335228d47ddcf6ae2f4c54987ee
Udaloiii <udaloiiisohappy@gmail.com>
2023-06-26 13:01:56 +0300

    Change style

--- src/ScrollToTop/CustomScrollToTop.tsx
+++ src/ScrollToTop/CustomScrollToTop.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ScrollToTop from "react-scroll-to-top";
-import s from './ScrollToTop.module.css'
+import s from './CustomScrollToTop.module.css'
 
 export const CustomScrollToTop = () => {
     return (
