c6f9af8e3230583ef5a6a966d98af3e1f15b9578
Udaloiii <udaloiiisohappy@gmail.com>
2023-06-27 12:20:27 +0300

    Tried to connect the styles for ScrollToTop through the usual css

--- src/ScrollToTop/CustomScrollToTop.tsx
+++ src/ScrollToTop/CustomScrollToTop.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ScrollToTop from "react-scroll-to-top";
-import s from './CustomScrollToTop.module.css'
+// import s from './CustomScrollToTop.module.css'
+import './CustomScrollToTop.css'
 
 export const CustomScrollToTop = () => {
     return (
-            <ScrollToTop smooth className={s.mainScrollBlock}/>
+            <ScrollToTop smooth className="mainScrollBlock"/>
     );
 };
