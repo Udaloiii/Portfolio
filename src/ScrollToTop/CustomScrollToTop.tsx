139f7acdc8751db99ecfd425a54ffdfc2e24f14d
Udaloiii <udaloiiisohappy@gmail.com>
2023-06-28 11:54:50 +0300

    Refactor

--- src/ScrollToTop/CustomScrollToTop.tsx
+++ src/ScrollToTop/CustomScrollToTop.tsx
@@ -1,18 +1,10 @@
 import React from 'react';
 import ScrollToTop from "react-scroll-to-top";
 import s from './CustomScrollToTop.module.css'
-// import './CustomScrollToTop.css'
+
 
 export const CustomScrollToTop = () => {
     return (
-        <ScrollToTop smooth
-                     // style={{
-                     //     "backgroundColor": "#157acc",
-                     //     "borderRadius": "35px",
-                     //     "width": "70px",
-                     //     "height": "70px",
-                     //     "right": "30px"
-                     // }}
-                     className={s.mainScrollBlock}/>
+        <ScrollToTop smooth className={s.mainScrollBlock}/>
     );
 };
