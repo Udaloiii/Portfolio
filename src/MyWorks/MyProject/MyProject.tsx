bc67750d523cdcd56f680be849af17c7903d8579
Udaloiii <udaloiiisohappy@gmail.com>
2023-07-04 20:08:52 +0300

    Added Title.tsx instead of simple title

--- src/MyWorks/MyProject/MyProject.tsx
+++ src/MyWorks/MyProject/MyProject.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import s from './MyProject.module.css'
+import {Title} from "../../Title/Title";
 
 export const MyProject = () => {
     return (
         <div className={s.mainProjectBlock}>
             <div className={s.imageBlock}>Image block</div>
             <div className={s.title}>
-                <h3>Title</h3>
+                {/*<h3>Title</h3>*/}
+                <Title value={"h3"} title={"title"}/>
             </div>
             <div className={s.description}>
                 Description
--- src/MyWorks/MyWorks.tsx
+++ src/MyWorks/MyWorks.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import s from './MyWorks.module.css'
 import {MyProject} from "./MyProject/MyProject";
+import {Title} from "../Title/Title";
 
 export const MyWorks = () => {
     return (
         <div className={s.main} id={"works"} >
             <div className={s.block}>
                 <div className={s.titleBlock}>
-                    <h2>My works</h2>
+                    {/*<h2>My works</h2>*/}
+                    <Title value={"h2"} title={"My works"}/>
                 </div>
                 <div className={s.mainBlock}>
                     <MyProject/>
