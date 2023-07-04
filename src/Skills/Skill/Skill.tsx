1fa8a7a3229ff34fb82eaa64c3fe119a07f00826
Udaloiii <udaloiiisohappy@gmail.com>
2023-07-04 20:11:53 +0300

    Changed style for Skill.module.css (main - changed size icons and removed description)

--- src/Skills/Skill/Skill.tsx
+++ src/Skills/Skill/Skill.tsx
@@ -1,25 +1,26 @@
 import React from 'react';
 import s from './Skill.module.css'
+import {Title} from "../../Title/Title";
 
 type SkillPropsType = {
     title: string
+    icon: string
 }
 export const Skill = (props: SkillPropsType) => {
-    const {title, ...otherProps} = props
+    const {title,icon, ...otherProps} = props
     return (
         <div className={s.mainSkillBlock}>
-            <h3 className={s.skillTitle}>
-                {title}
-            </h3>
+            {/*<h3 className={s.skillTitle}>*/}
+            {/*    {title}*/}
+            {/*</h3>*/}
+            <div className={s.skillTitle}>
+                <Title value={"h3"} title={title}/>
+            </div>
+
 
             <div className={s.imageBlock}>
-                <div className={s.image}>IMAGE</div>
-            </div>
-            <div className={s.description}>
-                Description
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. At aut commodi debitis, delectus dicta
-                doloribus earum eum expedita explicabo ipsa libero modi possimus quisquam, recusandae sit soluta sunt
-                suscipit, temporibus!
+                {/*<div className={s.image}>IMAGE</div>*/}
+                <img src={icon} alt={title} className={s.image}/>
             </div>
         </div>
     );
