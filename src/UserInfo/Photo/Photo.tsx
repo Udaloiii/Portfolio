459440e05e51103a527fa4cfef55e36d87496c5b
Udaloiii <udaloiiisohappy@gmail.com>
2023-09-08 20:50:33 +0300

    Changed photo style

--- src/UserInfo/Photo/Photo.tsx
+++ src/UserInfo/Photo/Photo.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import s  from './Photo.module.css'
-// import userPhoto from "../../Image/Screenshot_2021-04-19-22-09-33-748_com.mxtech.videoplayer.pro~2d.jpg";
-import userPhoto from "../../Image/astronaut.png";
+import s from './Photo.module.css'
+
+// import userPhoto from "../../Image/snapedit_obrez1.png";
 
 export const Photo = () => {
     // return <img className={s.main} src={userPhoto} alt="Eugene Udalykh"/>;
-    return <img className={s.main} src={userPhoto} alt="Eugene Udalykh"/>;
+    return <div className={s.main}/>;
 };
