5b0f157c4fa6f81bff390321358b0e2f15d336db
Udaloiii <udaloiiisohappy@gmail.com>
2023-11-08 19:10:25 +0300

    changed style for Skill.tsx; refactored

--- src/Skills/Skill/Skill.tsx
+++ src/Skills/Skill/Skill.tsx
@@ -7,19 +7,15 @@ type SkillPropsType = {
     icon: string
 }
 export const Skill = (props: SkillPropsType) => {
-    const {title,icon} = props
+    const {title, icon} = props
     return (
         <div className={s.mainSkillBlock}>
-            {/*<h3 className={s.skillTitle}>*/}
-            {/*    {title}*/}
-            {/*</h3>*/}
             <div className={s.skillTitle}>
                 <Title value={"h3"} title={title}/>
             </div>
 
 
             <div className={s.imageBlock}>
-                {/*<div className={s.image}>IMAGE</div>*/}
                 <img src={icon} alt={title} className={s.image}/>
             </div>
         </div>
