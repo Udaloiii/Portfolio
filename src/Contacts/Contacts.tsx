4416b974733e1a5ab0a7ca23b3bd7391b778ba5e
Udaloiii <udaloiiisohappy@gmail.com>
2023-11-17 22:10:48 +0300

    added library @emailjs/browser for send email; added function in Contacts.tsx

--- src/Contacts/Contacts.tsx
+++ src/Contacts/Contacts.tsx
@@ -1,23 +1,38 @@
-import React from 'react';
+import React, {ElementRef, FormEvent, useRef} from 'react';
 import s from './Contacts.module.css'
 import {Title} from "../Title/Title";
-
+import emailjs from '@emailjs/browser';
 
 export const Contacts = () => {
+    const form = useRef<ElementRef<'form'>>(null);
+
+    const sendEmail = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        if (!form.current) return
+
+        emailjs.sendForm('service_ui4gtul', 'template_l6zjclp', form.current, '69n7zQ3xC-JabiHJJ')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+        e.currentTarget.reset()
+    }
     return (
-        <footer className={s.mainFooter} id="contacts" >
+        <footer className={s.mainFooter} id="contacts">
 
             <div className={s.contacts}>
                 <Title value={"h2"} title={"Contacts"}/>
             </div>
 
-            <div className={s.inpTextArBlock}>
-                <input className={s.inputName} type="text" placeholder={"name"} />
-                <input className={s.inputEmail} type="email" placeholder={"e-mail"}/>
-                <textarea className={s.textArea} name="" id="" rows={7} placeholder={"your message"}></textarea>
-                <button className={s.buttonSend}>Send message</button>
-            </div>
+            <form className={s.inpTextArBlock} ref={form} onSubmit={sendEmail}>
+                <input className={s.inputName} required name={"name"} type="text" placeholder={"name"} autoComplete={"off"}/>
+                <input className={s.inputEmail} required name={"email"} type="email" placeholder={"e-mail"} autoComplete={"off"}/>
+                <textarea className={s.textArea} required name={"message"} id="" rows={7}
+                          placeholder={"your message"}></textarea>
+                <button className={s.buttonSend} type={"submit"}>Send message</button>
+            </form>
 
         </footer>
-    );
-};
+    )
+}
